2d7d5f3017b59ec7d7a8df349be844b997c172c1
cypherhack99 <58860117+cypherhack99@users.noreply.github.com>
2025-04-11 16:38:13 +0530

    xxxxxxxxxxx

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # install docker
-apt install docker.io docker-compose
-
-# install portainer (optional)
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo dnf install docker -y
 
 # Create folder structure for pretix
 mkdir -p /var/pretix-data
@@ -19,7 +15,7 @@ chown -R 15371:15371 /etc/pretix
 chmod 0700 /etc/pretix/pretix.cfg
 
 # Run docker compose file
-docker-compose up -d
+sudo docker-compose up -d
 
 # Create cron job
 (crontab -l 2>/dev/null; echo "15,45 * * * * /usr/bin/docker exec pretix.service pretix cron") | crontab -
